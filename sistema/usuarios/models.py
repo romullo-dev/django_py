e1beb7de4c2769fc1722a5792d181880bb046ad8
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2026-03-15 15:59:17 -0300

    commit

--- sistema/usuarios/models.py
+++ sistema/usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
--- sistema/usuarios/views.py
+++ sistema/usuarios/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Usuario
 
-# Create your views here.
+def lista_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'usuarios/lista.html', {'usuarios': usuarios})
